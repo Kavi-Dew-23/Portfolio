c32e026e0ad83f35b112a3b1066ee62143410292
Kavi-Dew-23 <dewapuraka@gmail.com>
2026-02-21 22:10:20 +0530

    Background update

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -5,12 +5,12 @@ const Navbar: React.FC = () => {
         <nav className="fixed top-5 right-5 p-5">
             <div className="flex gap-6 text-gray-300">
             {/**top bar */}
-            <a href="#home" className="no-underline hover:text-teal-500 transition">Home</a>
-            <a href="#about" className="no-underline hover:text-teal-500 transition">About</a>
-            <a href="#skills" className="no-underline hover:text-teal-500 transition">Skills</a>
-            <a href="#services" className="no-underline hover:text-teal-500 transition">Services</a>
-            <a href="#projects" className="no-underline hover:text-teal-500 transition">Projects</a>
-            <a href="#contact" className="no-underline hover:text-teal-500 transition">Contact</a>
+            <a href="#home" className="no-underline hover:text-teal-500 transition opacity-70">Home</a>
+            <a href="#about" className="no-underline hover:text-teal-500 transition opacity-70">About</a>
+            <a href="#skills" className="no-underline hover:text-teal-500 transition opacity-70">Skills</a>
+            <a href="#services" className="no-underline hover:text-teal-500 transition opacity-70">Services</a>
+            <a href="#projects" className="no-underline hover:text-teal-500 transition opacity-70">Projects</a>
+            <a href="#contact" className="no-underline hover:text-teal-500 transition opacity-70">Contact</a>
          </div>
         </nav>
         
